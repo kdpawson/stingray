67516b55f39b0447451ce6e15ccd3431ec7b1840
Keith <keith@garfish2.winnet.kdp>
2014-08-20 07:18:28 -0400

    adding who.sh script

---
+++ who.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+# Let me know who is currently logged in
+users="$( who | wc -l )"
+echo "There are currently $users users logged in."
+
+uniqeusers="$( who | cut -d" " -f1 | sort | uniq | wc -l )"
+echo "There are currently $uniqeusers individual users logged in."
+listusers="$( who | cut -d" " -f1 | sort | uniq )"
+
+echo "These peple are currently logged in: $listusers "
+
